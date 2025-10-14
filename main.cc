294530a58a0b83ab04391397dc86f6be7b4f3d8f
Rikuto Oishi <rikutooishi0911@gmail.com>
2025-10-15 08:58:01 +0900

    add float declarement

--- main.cc
+++ main.cc
@@ -2,6 +2,7 @@
 
 int main() {
     int i = 5;
+    float f = 3.1415; // 定義は可能
     printf("Hello, baremetal world! This is the %dth time of trying!\n", i);
     printf("Value of i: %d\n", i);
     while (1);
